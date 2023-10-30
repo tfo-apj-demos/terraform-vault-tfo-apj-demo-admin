a54c7d9ccc16fcb5ad43c1268c40349bfa1e4297
Grant Orchard <go@hashicorp.com>
2023-10-30 15:37:57 +1100

    add groups and entities

---
+++ 21 - identity_entity_groups.tf
@@ -0,0 +1,30 @@
+# Create external groups and associate them with Github aliases
+resource "vault_identity_group" "team_se" {
+  name     = "team-se"
+  type     = "external"
+  policies = [
+    "default",
+    "oidc"
+  ]
+}
+
+resource "vault_identity_group_alias" "team_se" {
+  name           = "team-se"
+  mount_accessor = vault_github_auth_backend.this.accessor
+  canonical_id   = vault_identity_group.team_se.id
+}
+
+resource "vault_identity_group" "apac_se" {
+  name     = "apac-se"
+  type     = "external"
+  policies = [
+    "default",
+    "oidc"
+  ]
+}
+
+resource "vault_identity_group_alias" "apac_se" {
+  name           = "apac-se"
+  mount_accessor = vault_github_auth_backend.this.accessor
+  canonical_id   = vault_identity_group.team_se.id
+}
