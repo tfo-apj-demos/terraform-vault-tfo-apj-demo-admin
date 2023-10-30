3327bc088aa4238490cdf51c665da14bf15d64c6
Grant Orchard <go@hashicorp.com>
2023-10-30 16:04:05 +1100

    configure oidc provider

---
+++ 22 - identity_oidc_provider.tf
@@ -0,0 +1,84 @@
+resource "vault_identity_oidc" "this" {
+  issuer = "https://production.vault.11eb56d6-0f95-3a99-a33c-0242ac110007.aws.hashicorp.cloud:8200"
+}
+
+resource "vault_identity_oidc_assignment" "team_se" {
+  name       = "team_se"
+  group_ids  = [
+    vault_identity_group.team_se.id,
+    vault_identity_group.apac_se.id,
+  ]
+  entity_ids = [ 
+    for v in vault_identity_entity.se: v.id
+  ]
+}
+
+resource "vault_identity_oidc_key" "team_se" {
+  name      = "team_se"
+  algorithm = "RS256"
+  verification_ttl = 7200
+  rotation_period = 7200
+}
+
+resource "vault_identity_oidc_client" "boundary" {
+  name          = "boundary"
+  redirect_uris = [
+    "https://e0078f32-6a05-44fe-a147-158f9d15a5fb.boundary.hashicorp.cloud/v1/auth-methods/oidc:authenticate:callback",
+    "http://127.0.0.1:8251/callback"
+  ]
+  assignments = [
+    vault_identity_oidc_assignment.team_se.name
+  ]
+  id_token_ttl     = 2400
+  access_token_ttl = 7200
+  key = vault_identity_oidc_key.team_se.id
+}
+
+resource "vault_identity_oidc_scope" "team_se" {
+  name        = "groups"
+  template    = "{\"groups\":{{identity.entity.groups.names}}}"
+}
+
+resource "vault_identity_oidc_provider" "team_se" {
+  name = "team_se"
+  https_enabled = true
+  issuer_host = "production.vault.11eb56d6-0f95-3a99-a33c-0242ac110007.aws.hashicorp.cloud:8200"
+  scopes_supported = [
+    vault_identity_oidc_scope.team_se.name
+  ]
+  allowed_client_ids = [
+    vault_identity_oidc_client.boundary.client_id
+  ]
+}
+
+resource "vault_identity_oidc_role" "team_se" {
+  name = "team_se"
+  key  = vault_identity_oidc_key.team_se.name
+  ttl = 60
+}
+
+resource "vault_identity_oidc_key_allowed_client_id" "team_se" {
+  key_name          = vault_identity_oidc_key.team_se.name
+  allowed_client_id = vault_identity_oidc_role.team_se.client_id
+}
+
+# resource "boundary_auth_method_oidc" "team_se" {
+#   scope_id = "global"
+#   issuer = vault_identity_oidc_provider.team_se.issuer
+#   client_id = vault_identity_oidc_client.boundary.client_id
+#   client_secret = vault_identity_oidc_client.boundary.client_secret
+#   signing_algorithms = ["RS256"]
+#   api_url_prefix = "https://e0078f32-6a05-44fe-a147-158f9d15a5fb.boundary.hashicorp.cloud"
+#   claims_scopes = [
+#     "groups",
+#     "user"
+#   ]
+#   name = "vault"
+#   state = "active-public"
+# }
+
+# resource "boundary_managed_group" "team_se" {
+#   name           = "team_se"
+#   auth_method_id = boundary_auth_method_oidc.team_se.id
+#   filter         = "name == \"team_se\" or name == \"apac_se\""
+# }
