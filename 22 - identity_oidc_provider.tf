1c1239cae29dac0273ba67062dd98f9c7b06f565
Grant Orchard <go@hashicorp.com>
2023-10-30 16:12:43 +1100

    clean up code

--- 22 - identity_oidc_provider.tf
+++ 22 - identity_oidc_provider.tf
@@ -61,24 +61,3 @@ resource "vault_identity_oidc_key_allowed_client_id" "team_se" {
   key_name          = vault_identity_oidc_key.team_se.name
   allowed_client_id = vault_identity_oidc_role.team_se.client_id
 }
-
-# resource "boundary_auth_method_oidc" "team_se" {
-#   scope_id = "global"
-#   issuer = vault_identity_oidc_provider.team_se.issuer
-#   client_id = vault_identity_oidc_client.boundary.client_id
-#   client_secret = vault_identity_oidc_client.boundary.client_secret
-#   signing_algorithms = ["RS256"]
-#   api_url_prefix = "https://e0078f32-6a05-44fe-a147-158f9d15a5fb.boundary.hashicorp.cloud"
-#   claims_scopes = [
-#     "groups",
-#     "user"
-#   ]
-#   name = "vault"
-#   state = "active-public"
-# }
-
-# resource "boundary_managed_group" "team_se" {
-#   name           = "team_se"
-#   auth_method_id = boundary_auth_method_oidc.team_se.id
-#   filter         = "name == \"team_se\" or name == \"apac_se\""
-# }
