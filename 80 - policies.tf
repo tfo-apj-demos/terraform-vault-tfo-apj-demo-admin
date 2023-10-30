3b1dcbb744e32fb4e1feb5b6dae6f8bfcc8b842c
Grant Orchard <go@hashicorp.com>
2023-10-30 20:20:08 +1100

    add role for tfc token generation

--- 80 - policies.tf
+++ 80 - policies.tf
@@ -1,16 +1,20 @@
 resource "vault_policy" "generate_tfc_token" {
     name = "generate_tfc_token"
-    path = vault_terraform_cloud_secret_role.boundary_admins.path
+    policy =<<EOH
+path = "${vault_terraform_cloud_secret_role.boundary_admins.path}" {
     capabilities = [
         "create",
         "read",
         "update",
         "delete"
-    ]
+    ]}
+EOH
 }
 
 resource "vault_policy" "auth_to_oidc" {
     name = "auth_to_oidc"
-    path = "identity/oidc/provider/team_se/authorize"
-    capabilities = [ "read" ] 
+    policy =<<EOH
+path = "identity/oidc/provider/team_se/authorize" {
+    capabilities = [ "read" ]
 }
+EOH
