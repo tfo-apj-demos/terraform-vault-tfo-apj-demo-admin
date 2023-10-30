ccefc4c7c8a17d478e1de13e56dddd74e1180607
Grant Orchard <go@hashicorp.com>
2023-10-30 13:15:35 +1100

    initial commit

---
+++ 00 - providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    vault = {
+      source = "hashicorp/vault"
+      version = "~> 3"
+    }
+  }
+}
+
+# provider "vault" {
+#   # Configuration options
+# }
