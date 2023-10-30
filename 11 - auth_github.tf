c755e6b588c738be8378533542fe0d40d0030629
Grant Orchard <go@hashicorp.com>
2023-10-30 15:28:34 +1100

    add github external groups for team-se and apac-se

--- 11 - auth_github.tf
+++ 11 - auth_github.tf
@@ -6,3 +6,35 @@ resource "vault_github_auth_backend" "this" {
     token_type = "default-service"
   }
 }
+
+resource "vault_identity_group" "team_se" {
+  name     = "team-se"
+  type     = "external"
+  policies = [
+    "default",
+    "oidc"
+  ]
+}
+
+resource "vault_identity_group_alias" "team_se" {
+  name           = "team-se"
+  mount_accessor = vault_github_auth_backend.this.accessor
+  canonical_id   = vault_identity_group.team_se.id
+}
+
+resource "vault_identity_group" "apac_se" {
+  name     = "apac-se"
+  type     = "external"
+  policies = [
+    "default",
+    "oidc"
+  ]
+}
+
+resource "vault_identity_group_alias" "apac_se" {
+  name           = "apac-se"
+  mount_accessor = vault_github_auth_backend.this.accessor
+  canonical_id   = vault_identity_group.team_se.id
+}
+
+
