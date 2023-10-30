6e6af6a84b112853860504bd912943dfc77bba17
Grant Orchard <go@hashicorp.com>
2023-10-30 18:31:34 +1100

    add outputs

---
+++ 99 - outputs.tf
@@ -0,0 +1,11 @@
+output "oidc_issuer" {
+  value = vault_identity_oidc_provider.team_se.issuer
+}
+
+output "oidc_client_id" {
+  value = vault_identity_oidc_client.boundary.client_id
+}
+
+output "oidc_client_secret" {
+  value = vault_identity_oidc_client.boundary.client_secret
+}
