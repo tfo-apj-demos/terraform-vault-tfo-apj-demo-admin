88f1e38343d35e21723e3848e81b5040370a34bd
Grant Orchard <go@hashicorp.com>
2023-10-30 18:25:56 +1100

    update tfe secrets engine

--- 30 - secrets_tfe.tf
+++ 30 - secrets_tfe.tf
@@ -1,15 +1,15 @@
-# resource "vault_terraform_cloud_secret_backend" "this" {
-#   token       = var.tfc_token
-# }
+resource "vault_terraform_cloud_secret_backend" "this" {
+  token       = var.tfc_token
+}
 
-# data "tfe_team" "boundary_admins" {
-#   organization = var.tfc_org_name
-#   name = "boundary_admins"
-# }
+data "tfe_team" "boundary_admins" {
+  organization = var.tfc_org_name
+  name = "boundary_admins"
+}
 
-# resource "vault_terraform_cloud_secret_role" "boundary_admins" {
-#   backend      = vault_terraform_cloud_secret_backend.this.backend
-#   name         = "boundary_admins"
-#   organization = var.tfc_org_name
-#   team_id      = data.tfe_team.boundary_admins.id
-# }
+resource "vault_terraform_cloud_secret_role" "boundary_admins" {
+  backend      = vault_terraform_cloud_secret_backend.this.backend
+  name         = "boundary_admins"
+  organization = var.tfc_org_name
+  team_id      = data.tfe_team.boundary_admins.id
+}
