5275afdee9171a24881f3eda5480a6593efbcb6f
Grant Orchard <go@hashicorp.com>
2023-11-01 16:11:51 +1100

    add apac-se members, create internal group for oidc passthrough

--- 20 - identity_entities.tf
+++ 20 - identity_entities.tf
@@ -1,8 +1,8 @@
 locals {
-  github_usernames = flatten(
+  github_usernames = flatten(concat(
     [ for team in data.github_organization_teams.team_se.teams: team.members if team.name == "team-se" ],
     [ for team in data.github_organization_teams.team_se.teams: team.members if team.name == "team-apac" ]
-  )
+  ))
 }
 
 # Lookup our GitHub org for teams and memberships
