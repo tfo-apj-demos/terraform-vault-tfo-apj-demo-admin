d0efdbc2e6e34f46ea5d300a6bfa52874efe4c30
Grant Orchard <go@hashicorp.com>
2023-11-01 14:41:58 +1100

    update callback url

--- 22 - identity_oidc_provider.tf
+++ 22 - identity_oidc_provider.tf
@@ -23,8 +23,7 @@ resource "vault_identity_oidc_key" "team_se" {
 resource "vault_identity_oidc_client" "boundary" {
   name          = "boundary"
   redirect_uris = [
-    "${boundary_auth_method_oidc.team_se.callback_url}",
-    #"https://8b596635-91df-45a3-8455-1ecbf5e8c43e.boundary.hashicorp.cloud/v1/auth-methods/oidc:authenticate:callback",
+    "https://8b596635-91df-45a3-8455-1ecbf5e8c43e.boundary.hashicorp.cloud/v1/auth-methods/oidc:authenticate:callback",
     "http://127.0.0.1:8251/callback"
   ]
   assignments = [
