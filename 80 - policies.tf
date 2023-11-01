0d40947dfcfb5f837cdb6b956cd8681799ca1205
Grant Orchard <go@hashicorp.com>
2023-11-01 14:58:26 +1100

    add interpolation to policy path to prevent fat fingered mistakes

--- 80 - policies.tf
+++ 80 - policies.tf
@@ -14,7 +14,7 @@ EOH
 resource "vault_policy" "auth_to_oidc" {
     name = "auth_to_oidc"
     policy =<<EOH
-path "identity/oidc/provider/team_se/authorize" {
+path "${vault_identity_oidc_provider.team_se.id}" {
     capabilities = [ "read" ]
 }
 EOH
