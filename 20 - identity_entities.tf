efc08fd12fa2efaaa685c9e80b931e34f4fa9b79
Grant Orchard <go@hashicorp.com>
2023-11-01 17:01:25 +1100

    add correct name for apac-se team

--- 20 - identity_entities.tf
+++ 20 - identity_entities.tf
@@ -1,7 +1,7 @@
 locals {
   github_usernames = flatten(concat(
     [ for team in data.github_organization_teams.team_se.teams: team.members if team.name == "team-se" ],
-    [ for team in data.github_organization_teams.team_se.teams: team.members if team.name == "team-apac" ]
+    [ for team in data.github_organization_teams.team_se.teams: team.members if team.name == "apac-se" ]
   ))
 }
 
