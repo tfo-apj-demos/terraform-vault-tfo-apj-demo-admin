39fb77e8cdcd67991b3b3205d88b355a04f1c392
Grant Orchard <go@hashicorp.com>
2023-12-22 12:15:54 +1100

    cleanup unneeded code

--- 80 - policies.tf
+++ 80 - policies.tf
@@ -1,54 +1,8 @@
-
-# resource "vault_policy" "generate_tfc_token" {
-#     name = "generate_tfc_token"
-#     policy =<<EOH
-# path "terraform/creds/boundary_admins" {
-#     capabilities = [
-#         "create",
-#         "read",
-#         "update",
-#         "delete"
-#     ]}
-# EOH
-# }
-
+# --- Loop over the policy directory and create policies
+# --- that match the file names in it.
 resource "vault_policy" "this" {
     for_each = fileset("${path.module}/policies", "*.hcl")
     name = trimsuffix(each.value, ".hcl")
     policy = file("${path.module}/policies/${each.value}")
 }   
 
-# resource "vault_policy" "auth_to_oidc" {
-#     name = "auth_to_oidc"
-#     policy =<<EOH
-# path "identity/oidc/provider/team_se/authorize" {
-#     capabilities = [ "read" ]
-# }
-# EOH
-# }
-
-# resource "vault_policy" "ssh_sign" {
-#     name = "sign_ssh_certificate"
-#     policy =<<EOH
-# path "ssh/sign/boundary" {
-#   capabilities = ["read", "update"]
-# }
-# EOH
-# }
-
-# resource "vault_policy" "revoke_lease" {
-#     name = "revoke_lease"
-#     policy =<<EOH
-# path "sys/leases/revoke" {
-#     capabilities = ["update"]
-# }
-# EOH
-# }
-
-# resource "vault_policy" "manage_pki" {
-#     name = "manage_pki"
-#     policy =<<EOH
-# path "pki/intermediate/set-signed" {
-#     capabilities = ["update"]
-# }
-# }
