ecfb0eb24613d778043b47ada3e272b323f01d96
Grant Orchard <go@hashicorp.com>
2023-12-29 09:26:02 +1100

    add transit management

--- 33 - secrets_transit.tf
+++ 33 - secrets_transit.tf
@@ -1,21 +1,21 @@
-import {
-  id = "transit"
-  to = vault_mount.transit
-}
+# import {
+#   id = "transit"
+#   to = vault_mount.transit
+# }
 
-resource "vault_mount" "transit" {
-  path                      = "transit"
-  type                      = "transit"
-  default_lease_ttl_seconds = 3600
-  max_lease_ttl_seconds     = 86400
-}
+# resource "vault_mount" "transit" {
+#   path                      = "transit"
+#   type                      = "transit"
+#   default_lease_ttl_seconds = 3600
+#   max_lease_ttl_seconds     = 86400
+# }
 
-import {
-  id = "transit/vault_unseal"
-  to = vault_transit_secret_backend_key.vault_unseal
-}
+# import {
+#   id = "transit/vault_unseal"
+#   to = vault_transit_secret_backend_key.vault_unseal
+# }
 
-resource "vault_transit_secret_backend_key" "vault_unseal" {
-  backend = vault_mount.transit.path
-  name    = "vault_unseal"
-}
+# resource "vault_transit_secret_backend_key" "vault_unseal" {
+#   backend = vault_mount.transit.path
+#   name    = "vault_unseal"
+# }
