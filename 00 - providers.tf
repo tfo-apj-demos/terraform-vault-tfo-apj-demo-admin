91e5139721cba67350a98cf38ffe19cbd9a8c5e3
Grant Orchard <go@hashicorp.com>
2024-05-22 12:24:55 +1000

    bump vault provider

--- 00 - providers.tf
+++ 00 - providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     vault = {
       source = "hashicorp/vault"
-      version = "~> 3"
+      version = "~> 4"
     }
     github = {
       source = "integrations/github"
