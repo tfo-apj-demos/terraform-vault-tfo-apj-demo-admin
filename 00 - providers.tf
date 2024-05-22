ecb87a1e1c408ab8a2fbdd6f66c3c334301a6fbf
Grant Orchard <go@hashicorp.com>
2024-05-22 11:20:59 +1000

    update provider config to use service account

--- 00 - providers.tf
+++ 00 - providers.tf
@@ -20,13 +20,16 @@ terraform {
   cloud {
     organization = "tfo-apj-demos"
     workspaces {
-      name = "hcp-vault-config"
+      name = "vault-hcp-config"
     }
   }
 }
 
 provider "boundary" {
-  addr                   = var.boundary_addr
+  addr                   = var.boundary_address
+  auth_method_id         = var.service_account_authmethod_id
+  auth_method_login_name = var.service_account_name
+  auth_method_password   = var.service_account_password
 }
 
 provider "vault" {
--- 90 - variables.tf
+++ 90 - variables.tf
@@ -12,7 +12,19 @@ variable "tfc_token" {
   type = string
 }
 
-variable "boundary_addr" {
+variable "boundary_address" {
+  type = string
+}
+
+variable "service_account_authmethod_id" {
+  type = string
+}
+
+variable "service_account_name" {
+  type = string
+}
+
+variable "service_account_password" {
   type = string
 }
 
