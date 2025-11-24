f73ccf0d46d852d454c4ae5fe4e7455acb01ed50
Aaron Evans <aarone@outlook.com.au>
2025-11-24 16:21:18 +1100

    Comment out the PKI root module configuration for future reference

--- 32 - secrets_pki.tf
+++ 32 - secrets_pki.tf
@@ -28,10 +28,10 @@ EOH
 }
 
 
-module "pki_root" {
-  source = "./modules/pki/root"
+# module "pki_root" {
+#   source = "./modules/pki/root"
   
-  root_ca_cert         = var.root_ca_cert
-  root_ca_key          = var.root_ca_key
-  organization_domains = ["hashicorp.local"]
-}
+#   root_ca_cert         = var.root_ca_cert
+#   root_ca_key          = var.root_ca_key
+#   organization_domains = ["hashicorp.local"]
+# }
