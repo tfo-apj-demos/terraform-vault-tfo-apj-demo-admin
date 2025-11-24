5a38222ea52bfc98f9c322cf4e32b1fd06ec9dd9
Aaron Evans <aarone@outlook.com.au>
2025-11-24 14:39:50 +1100

    Update vault provider version to ~> 5

--- 00 - providers.tf
+++ 00 - providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     vault = {
       source = "hashicorp/vault"
-      version = "~> 4"
+      version = "~> 5"
     }
     github = {
       source = "integrations/github"
