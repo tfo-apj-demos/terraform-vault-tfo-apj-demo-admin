b8ace921533289f4cad5e852c59ea2ed9c43281b
Aaron Evans <aarone@outlook.com.au>
2025-11-24 15:37:48 +1100

    Downgrade vault provider version to ~> 4 for compatibility

--- 00 - providers.tf
+++ 00 - providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     vault = {
       source = "hashicorp/vault"
-      version = "~> 5"
+      version = "~> 4"
     }
     github = {
       source = "integrations/github"
