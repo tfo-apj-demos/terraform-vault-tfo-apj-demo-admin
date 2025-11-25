e5799acc5ab13252b70fde16c0218984b0bff1d3
Aaron Evans <aarone@outlook.com.au>
2025-11-25 21:10:08 +1100

    Remove unused namespace variables from central signing module

--- modules/pki/central-signing/variables.tf
+++ modules/pki/central-signing/variables.tf
@@ -1,15 +1,3 @@
-variable "namespace" {
-  description = "The namespace to provision the central signing CA in"
-  type        = string
-  default     = "admin"
-}
-
-variable "root_ca_namespace" {
-  description = "The namespace where the root CA is located (use null for root namespace)"
-  type        = string
-  default     = null
-}
-
 variable "root_ca_backend_path" {
   description = "The backend path of the root CA"
   type        = string
